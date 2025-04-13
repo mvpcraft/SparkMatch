d0a22e5b942e04ddccb77fa36be43559854a68a7
mvpcraft <goldstar105000117@gmail.com>
2025-04-13 22:51:54 +0530

    updated UI.

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,50 @@
-import 'package:dating_app/presentation/features/user/home/user_home_screen.dart';
+// import 'package:dating_app/presentation/features/user/home/user_home_screen.dart';
+// import 'package:flutter/material.dart';
+
+// void main() {
+//   runApp(const MyApp());
+// }
+
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//         title: 'SparkMatch',
+//         debugShowCheckedModeBanner: false,
+//         theme: ThemeData(
+//           primarySwatch: Colors.pink,
+//           visualDensity: VisualDensity.adaptivePlatformDensity,
+//         ),
+//         home: HomeScreen());
+//   }
+// }
+import 'package:dating_app/presentation/screens/home_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const SparkMatchApp());
 }
 
-class MyApp extends StatefulWidget {
-  const MyApp({super.key});
+class SparkMatchApp extends StatelessWidget {
+  const SparkMatchApp({super.key});
 
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: 'SparkMatch',
       debugShowCheckedModeBanner: false,
-      home: UserHomeScreen(),
+      theme: ThemeData(
+        primarySwatch: Colors.pink,
+        scaffoldBackgroundColor: Colors.white,
+        appBarTheme: const AppBarTheme(
+          elevation: 0,
+          backgroundColor: Colors.white,
+          iconTheme: IconThemeData(color: Colors.black54),
+        ),
+      ),
+      home: const HomeScreen(),
     );
   }
 }
---
+++ lib/presentation/screens/home_screen.dart
@@ -0,0 +1,60 @@
+import 'package:dating_app/presentation/screens/discover_screen.dart';
+import 'package:dating_app/presentation/screens/explore_screen.dart';
+import 'package:dating_app/presentation/screens/matches_screen.dart';
+import 'package:dating_app/presentation/screens/profile_screen.dart';
+import 'package:flutter/material.dart';
+
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int _currentIndex = 0;
+
+  final List<Widget> _screens = [
+    const DiscoverScreen(),
+    const MatchesScreen(),
+    const ExploreScreen(),
+    const ProfileScreen(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _screens[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        onTap: (index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+        type: BottomNavigationBarType.fixed,
+        selectedItemColor: const Color(0xFFFE3C72),
+        unselectedItemColor: Colors.grey,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home_filled),
+            label: 'Discover',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.favorite_border),
+            label: 'Matches',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.explore_outlined),
+            label: 'Explore',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person_outline),
+            label: 'Profile',
+          ),
+        ],
+      ),
+    );
+  }
+}
